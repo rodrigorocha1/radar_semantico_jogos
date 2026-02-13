78043066a48db1e343c1068c4eb522515d4c3fd8
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-13 19:58:38 -0300

    feat: adionando verificação da conexão da api da steam

---
+++ src/corrente_pipeline_comentarios/verificar_conexao_api_steam_corrente.py
@@ -0,0 +1,12 @@
+from src.corrente_pipeline_comentarios.corrente import Corrente
+from src.servicos.steam.iapi_steam import IAPISteam
+
+
+class VerificarConexaoApiSteamCorrente(Corrente):
+
+    def __init__(self, steam_api: IAPISteam):
+        super().__init__()
+        self.__steam_api = steam_api
+
+    def executar_processo(self, contexto) -> bool:
+        return self.__steam_api.checar_conexao()
