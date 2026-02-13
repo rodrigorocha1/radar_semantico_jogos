75945d5da92a1f20bc4d5fb31cb5ad53b6b44b8d
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-12 22:39:52 -0300

    style: Adicionei comentários dos métodos

--- src/servicos/api_youtube/i_steam/iapi_steam.py
+++ src/servicos/api_youtube/i_steam/iapi_steam.py
@@ -4,9 +4,21 @@ from typing import List
 
 class IAPISteam(ABC):
     @abstractmethod
-    def checar_conexao(self, id_video: str) -> bool:
+    def checar_conexao(self) -> bool:
+        """
+        Método para checar a conexão
+        :return: True se conexão é um sucesso falso caso contrário
+        :rtype: bool
+        """
         pass
 
     @abstractmethod
-    def obter_dados(self, id_jogo: int) -> List:
+    def obter_comentarios(self, id_jogo: int) -> List:
+        """
+        Método para obter o comentários de um jogo
+        :param id_jogo: id do jogo
+        :type id_jogo: int
+        :return: A lista de comentários
+        :rtype: List
+        """
         pass
