be860d93fcce2cc0df4205e39365a8fd7fd3416c
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-12 22:09:45 -0300

    v1

---
+++ corrente_pipeline_comentarios/corrente.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+class Corrente(ABC):
+    def __init__(self) -> None:
+        self._proxima_corrente : Optional["Corrente"] = None
+
+    def set_proxima_corrente(self, corrente: "Corrente") :
+        self._proxima_corrente = corrente
+        return corrente
+
+    def corrente(self, contexto):
+        if self.executar_processo(contexto):
+            print('Sucesso')
+        else:
+            print('Falha')
+
+    @abstractmethod
+    def executar_processo(self, contexto) -> bool:
+        pass
