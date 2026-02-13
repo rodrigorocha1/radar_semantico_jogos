c057c48492375b1771f4f2ee04cbca5a359f39dc
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-12 23:10:28 -0300

    chore: Atualizando log para registro detalhado

--- src/servicos/config/configuracao_log.py
+++ src/servicos/config/configuracao_log.py
@@ -1,19 +1,25 @@
 import logging
 from pathlib import Path
+
 log_dir = Path(__file__).resolve().parents[3] / "log"
 log_dir.mkdir(exist_ok=True)
 
 log_file = log_dir / "steam_api.log"
 
-
-
-logger = logging.getLogger("steam_api_checker")
+logger = logging.getLogger("pipeline extração comentários")
 logger.setLevel(logging.INFO)
 
 # Handler para arquivo
 file_handler = logging.FileHandler(log_file)
 file_handler.setLevel(logging.INFO)
-file_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S")
+file_formatter = logging.Formatter(
+    "%(asctime)s | [%(levelname)s] |  "
+    "%(filename)s | %(funcName)s() | %(lineno)d | %(pathname)s | "
+    "%(name)s | %(module)s | "
+    "%(threadName)s(%(thread)d) | %(processName)s(%(process)d) - "
+    "%(message)s",
+    "%Y-%m-%d %H:%M:%S"
+)
 file_handler.setFormatter(file_formatter)
 
 # Handler para terminal
