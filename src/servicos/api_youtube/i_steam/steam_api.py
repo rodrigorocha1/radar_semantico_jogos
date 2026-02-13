8757781723e7a74dd8a70f236a25d460797e4fa5
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-12 22:54:37 -0300

    feat: Adicionei o método de checar conexão na api da steam

--- src/servicos/api_youtube/i_steam/steam_api.py
+++ src/servicos/api_youtube/i_steam/steam_api.py
@@ -1,15 +1,42 @@
+import subprocess
 from typing import List, Final
 
 from src.servicos.api_youtube.i_steam.iapi_steam import IAPISteam
 from src.servicos.config.config import Config
 
 
+# adicionar log
+
 class SteamAPI(IAPISteam):
     def __init__(self):
         self.__url_base: Final[str] = Config.STEAM_API_URL
 
     def checar_conexao(self) -> bool:
-        pass
+        """
+            Método para checar a conexão
+            :return: True se conexão é um sucesso falso caso contrário
+            :rtype: bool
+        """
+
+        try:
+            subprocess.run(
+                ["curl", "-s", "-f", self.__url_base],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
 
     def obter_comentarios(self, id_jogo: int) -> List:
+        """
+            Método para obter o comentários de um jogo
+            :param id_jogo: id do jogo
+            :type id_jogo: int
+            :return: A lista de comentários
+            :rtype: List
+        """
         pass
+
