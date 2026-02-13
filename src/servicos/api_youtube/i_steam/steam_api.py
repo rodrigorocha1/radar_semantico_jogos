8cd70b2b660a7c2bd3c06a15cc50fab68c625c49
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-12 22:48:05 -0300

    feat: Adicionei a url base da steam

--- src/servicos/api_youtube/i_steam/steam_api.py
+++ src/servicos/api_youtube/i_steam/steam_api.py
@@ -1,9 +1,13 @@
-from typing import List
+from typing import List, Final
 
 from src.servicos.api_youtube.i_steam.iapi_steam import IAPISteam
+from src.servicos.config.config import Config
 
 
 class SteamAPI(IAPISteam):
+    def __init__(self):
+        self.__url_base: Final[str] = Config.STEAM_API_URL
+
     def checar_conexao(self) -> bool:
         pass
 
