268ea4d41965f4c64ce96370a889d0e8f42bf44e
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 20:54:49 -0300

    chorus: Adicionei dataframe original

--- src/contexto/contexto.py
+++ src/contexto/contexto.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass, field
 from typing import Iterable, Any, List, Tuple
 
+import pandas as pd
+
 
 @dataclass
 class Contexto:
     gerador_reviews_steam: Iterable[Any] = field(default_factory=list)
     gerador_comentarios_youtube: Iterable[Any] = field(default_factory=list)
     lista_id_comentarios: List[Tuple[str, ...]] = field(default_factory=list)
-    gerador_resposta_comentarios : Iterable[Any] = field(default_factory=list)
+    gerador_resposta_comentarios: Iterable[Any] = field(default_factory=list)
+    dataframe_original: pd.DataFrame = field(default_factory=pd.DataFrame())
