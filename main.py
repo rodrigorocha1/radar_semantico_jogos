29085aa74f333e073f771e4e1f77d74ad7df0be6
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 14:10:57 -0300

    feat: esqueleto principal

--- main.py
+++ main.py
@@ -13,9 +13,18 @@ from src.servicos.api_youtube.api_youtube import YoutubeAPI
 from src.servicos.banco.operacoes_banco import OperacoesBancoDuckDb
 from src.servicos.servico_s3.sevicos3 import ServicoS3
 from src.servicos.steam.steam_api import SteamAPI
+'''
+1631270 - Star Rupture
+275850 - No Man's Sky
+392160 - X4: Foundations
+526870 - Satisfactory
+1284190 - Planet Crafter
+4078590- The Planet Crafter - Toxicity
 
+275850, 392160, 526870,  1284190
+'''
 contexto = Contexto()
-lista_jogos = [1631270, 275850]
+lista_jogos = [1631270]
 lista_jogos_youtube = [('tg51dCB_WbE', 'windows')]
 steam_api = SteamAPI()
 servico_s3 = ServicoS3()
@@ -23,7 +32,7 @@ servico_banco = OperacoesBancoDuckDb()
 api_youtube = YoutubeAPI()
 
 p1 = VerificarConexaoApiSteamCorrente(steam_api=steam_api)
-p2 = ObterComentariosSteamCorrente(api_steam=SteamAPI(), lista_jogos=lista_jogos)
+p2 = ObterComentariosSteamCorrente(api_steam=steam_api, lista_jogos=lista_jogos)
 p3 = GuardarDadosSteam3Corrente(
     servico_s3=servico_s3,
     servico_banco=servico_banco
@@ -46,6 +55,10 @@ p7 = GuardarDadosYoutubeRespostaComentariosS3Corrente(
     servico_banco=servico_banco
 )
 
-p1.set_proxima_corrente(p2).set_proxima_corrente(p3).set_proxima_corrente(p4).set_proxima_corrente(
-    p5).set_proxima_corrente(p6).set_proxima_corrente(p7)
+# p1.set_proxima_corrente(p2).set_proxima_corrente(p3).set_proxima_corrente(p4).set_proxima_corrente(
+#     p5).set_proxima_corrente(p6).set_proxima_corrente(p7)
+# p1.corrente(contexto=contexto)
+
+
+p1.set_proxima_corrente(p2).set_proxima_corrente(p3)
 p1.corrente(contexto=contexto)
