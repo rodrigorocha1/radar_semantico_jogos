81280b98dfe6dd6184db7788bf8b443e28686fef
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 13:35:48 -0300

    fix : correção

--- src/contexto/contexto.py
+++ src/contexto/contexto.py
@@ -7,3 +7,4 @@ class Contexto:
     gerador_reviews_steam: Iterable[Any] = field(default_factory=list)
     gerador_comentarios_youtube: Iterable[Any] = field(default_factory=list)
     lista_id_comentarios: List[Tuple[str, ...]] = field(default_factory=list)
+    gerador_resposta_comentarios : Iterable[Any] = field(default_factory=list)
