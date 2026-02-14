073079fc8503be083fabd9d61bc2421d4432ea7c
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-13 23:00:29 -0300

    feat: criaçção da variavel de ambiente para url do minio s3

--- src/servicos/config/config.py
+++ src/servicos/config/config.py
@@ -8,8 +8,9 @@ load_dotenv()
 
 class Config:
     STEAM_API_URL: Final[str] = os.getenv("STEAM_API_URL", " ")
-    MINIO_ENDPOINT: Final[str] = os.getenv("MLFLOW_S3_ENDPOINT_URL", " ")
+    MINIO_ENDPOINT: Final[str] = os.getenv("MINIO_HOST_URL", " ")
     MINIO_ACCESS_KEY: Final[str] = os.getenv("MINIO_ROOT_USER", " ")
     MINIO_SECRET_KEY: Final[str] = os.getenv("MINIO_ROOT_PASSWORD", " ")
     MINIO_BUCKET: Final[str] = os.getenv("MINIO_BUCKET")
     AWS_REGION: Final[str] = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
+    MINIO_BUCKET_PLN: Final[str] = os.getenv("MINIO_BUCKET_PLN", "")
