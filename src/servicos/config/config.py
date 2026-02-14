c28110a70d192712895cd1a2f1340ce8b8784f33
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 20:51:49 -0300

    feat: adicionei config jogos na config

--- src/servicos/config/config.py
+++ src/servicos/config/config.py
@@ -1,5 +1,5 @@
 import os
-from typing import Final
+from typing import Final, Dict
 
 from dotenv import load_dotenv
 
@@ -16,3 +16,27 @@ class Config:
     MINIO_BUCKET_PLN: Final[str] = os.getenv("MINIO_BUCKET_PLN", "")
     MINIO_HOST_URL_DUCKDB: Final[str] = os.getenv("MINIO_HOST_URL_DUCKDB", "")
     CHAVE_API_YOUTUBE: Final[str] = os.getenv("CHAVE_API_YOUTUBE", "")
+    CONFIG_JOGOS: Final[Dict[str, int]] = {
+        "star_rupture": 1631270,
+        "no_mans_sky": 275850,
+        "x4_foundations": 392160,
+        "satisfactory": 526870,
+        "planet_crafter": 1284190,
+        "the_planet_crafter_toxicity": 4078590,
+        "the_planet_crafter_planet_humble": 3142540,
+        "elite_dangerous": 359320,
+        "elite_dangerous_odissey": 1336350,
+        "euro_truck_simulator": 227300,
+        "euro_truck_simulator_grecia": 2604420,
+        "euro_truck_simulator_iberia": 1209460,
+        "euro_truck_simulator_italia": 558244,
+        "euro_truck_simulator_beyound_the_baltic_sea": 925580,
+        "euro_truck_simulator_road_to_the_black_sea": 1056760,
+        "euro_truck_simulator_vive_le_france": 531130,
+        "euro_truck_simulator_scandinaavia": 304212,
+        "euro_truck_simulator_going_east": 227310,
+        "euro_truck_simulator_nordic_horizons": 2780810,
+        "space_engineers": 244850,
+        "cities_skylines": 255710,
+        "cities_skylines_dois": 949230,
+    }
