7cd685dcea6e6808d0983f79fe8b7e22eae7b7b8
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 20:56:31 -0300

    fix: remoção de parenteses de Dataframe de dataframe_original

--- src/contexto/contexto.py
+++ src/contexto/contexto.py
@@ -10,4 +10,4 @@ class Contexto:
     gerador_comentarios_youtube: Iterable[Any] = field(default_factory=list)
     lista_id_comentarios: List[Tuple[str, ...]] = field(default_factory=list)
     gerador_resposta_comentarios: Iterable[Any] = field(default_factory=list)
-    dataframe_original: pd.DataFrame = field(default_factory=pd.DataFrame())
+    dataframe_original: pd.DataFrame = field(default_factory=pd.DataFrame)
