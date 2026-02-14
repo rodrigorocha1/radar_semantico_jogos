01d79b76df1ce7de89971f3325496480cca7e4c6
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 12:52:14 -0300

    fix: tratamento de exceção do duck db

--- src/corrente_pipeline_comentarios/guardar_dados_comentarios_youtube_s3_corrente.py
+++ src/corrente_pipeline_comentarios/guardar_dados_comentarios_youtube_s3_corrente.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from typing import Optional
+import duckdb
+import pandas as pd
 
 from src.contexto.contexto import Contexto
 from src.corrente_pipeline_comentarios.corrente import Corrente
@@ -8,27 +10,39 @@ from src.servicos.config.configuracao_log import logger
 from src.servicos.servico_s3.iservicos3 import Iservicos3
 
 
-class GuardarDadosYoutubeS3Corrente(Corrente):
+class GuardarDadosYoutubeComentariosS3Corrente(Corrente):
 
     def __init__(self, servico_s3: Iservicos3, servico_banco: IoperacoesBanco):
         super().__init__()
         self.__servico_s3 = servico_s3
         self.__caminho_data = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        self.__caminho_arquivo = f'youtube/bronze/comentarios_youtube/'
+        self.__caminho_arquivo = f'youtube/bronze/comentarios_youtube/jogo_'
         self.__servico_banco = servico_banco
 
     def executar_processo(self, contexto: Optional[Contexto] = None) -> bool:
         for dados in contexto.gerador_comentarios_youtube:
+
             id_comentario = dados['id']
             data_atualizacao_api = dados['snippet']['topLevelComment']['snippet']['updatedAt']
+            caminho_base = f"{self.__caminho_arquivo}{dados['nome_jogo']}/*/*/*.json"
 
-            condicao = f"id = {id_comentario} AND snippet.topLevelComment.snippet.updatedAt = {data_atualizacao_api}"
-            caminho_consulta = f"s3://extracao/youtube/bronze/comentarios_youtube/{dados['nome_jogo']}/{dados['snippet']['channelId']}/{dados['snippet']['videoId']}/*.json"
+            condicao = f"id = '{id_comentario}' AND snippet.topLevelComment.snippet.updatedAt = '{data_atualizacao_api}'"
+            caminho_consulta = f"s3://extracao/{caminho_base}"
+            try:
+                dataframe = self.__servico_banco.consultar_dados(caminho_consulta=caminho_consulta,
+                                                                 id_consulta=condicao)
+            except duckdb.IOException as e:
+                logger.error(f'{e}')
+                logger.info(f'{id_comentario}  error atualizacao')
+                dataframe = pd.DataFrame()
 
-            dataframe = self.__servico_banco.consultar_dados(caminho_consulta=caminho_consulta, id_consulta=condicao)
             if dataframe.empty:
-                caminho_completo = self.__caminho_arquivo + f'jogo_{dados["codigo_steam"]}/' + f'data_{self.__caminho_data}' + '_reviews.json'
-                self.__servico_s3.guardar_dados(dados, caminho_completo)
+                logger.info(f'{id_comentario}  teve atualizacao')
+                caminho_arquivo = f"{self.__caminho_arquivo}{dados['nome_jogo']}/{dados['snippet']['channelId']}/{dados['snippet']['videoId']}/*.json"
+
+                self.__servico_s3.guardar_dados(dados, caminho_arquivo)
             else:
                 logger.info(f'{id_comentario} não teve atualizacao')
+
+
         return True
