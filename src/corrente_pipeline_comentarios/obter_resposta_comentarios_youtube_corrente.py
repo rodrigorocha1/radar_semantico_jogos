cb03cef0a7e389bf9af7f636cd6f735a7646178b
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 13:14:11 -0300

    refactor: remoção if else desnecessário

--- src/corrente_pipeline_comentarios/obter_resposta_comentarios_youtube_corrente.py
+++ src/corrente_pipeline_comentarios/obter_resposta_comentarios_youtube_corrente.py
@@ -12,19 +12,15 @@ class ObterRespostaComentariosYoutubeCorrente(Corrente):
         super().__init__()
 
     def executar_processo(self, contexto: Optional[Contexto] = None) -> bool:
-        if contexto is None:
-            return False
-        if contexto.gerador_comentarios_youtube is not None:
-            gerador_reviews = chain.from_iterable(
-                map(
-                    lambda review: {**review, 'nome_jogo': comentario['nome_jogo'],
-                                    'id_video': comentario.get('id_video')},
-                    self.__api_youtube.obter_resposta_comentarios(
-                        id_comentario=comentario['id']
-                    )
+        gerador_reviews = chain.from_iterable(
+            map(
+                lambda review: {**review, 'nome_jogo': comentario['nome_jogo'],
+                                'id_video': comentario.get('id_video')},
+                self.__api_youtube.obter_resposta_comentarios(
+                    id_comentario=comentario['id']
                 )
-                for comentario in contexto.gerador_comentarios_youtube
             )
-            contexto.gerador_resposta_comentarios_youtube = gerador_reviews
-            return True
-        return False
+            for comentario in contexto.gerador_comentarios_youtube
+        )
+        contexto.gerador_resposta_comentarios_youtube = gerador_reviews
+        return True
