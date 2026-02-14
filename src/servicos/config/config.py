626e40726fe7af011294952744f30154899f7803
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-13 22:40:55 -0300

    feat: adicionando chaves minio s3

--- src/servicos/config/config.py
+++ src/servicos/config/config.py
@@ -8,3 +8,8 @@ load_dotenv()
 
 class Config:
     STEAM_API_URL: Final[str] = os.getenv("STEAM_API_URL", " ")
+    MINIO_ENDPOINT: Final[str] = os.getenv("MLFLOW_S3_ENDPOINT_URL", " ")
+    MINIO_ACCESS_KEY: Final[str] = os.getenv("MINIO_ROOT_USER", " ")
+    MINIO_SECRET_KEY: Final[str] = os.getenv("MINIO_ROOT_PASSWORD", " ")
+    MINIO_BUCKET: Final[str] = os.getenv("MINIO_BUCKET")
+    AWS_REGION: Final[str] = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
