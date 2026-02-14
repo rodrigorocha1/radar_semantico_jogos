c5c47ac6288de35e1b22607bea009c13461a7680
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-14 00:47:38 -0300

    feat: Verificando registros repertidos

--- src/corrente_pipeline_comentarios/guardar_dados_steam_s3_corrente.py
+++ src/corrente_pipeline_comentarios/guardar_dados_steam_s3_corrente.py
@@ -3,20 +3,33 @@ from typing import Optional
 
 from src.contexto.contexto import Contexto
 from src.corrente_pipeline_comentarios.corrente import Corrente
+from src.servicos.banco.ioperacoes_banco import IoperacoesBanco
+from src.servicos.config.configuracao_log import logger
 from src.servicos.servico_s3.iservicos3 import Iservicos3
 
 
 class GuardarDadosSteam3Corrente(Corrente):
 
-    def __init__(self, servico_s3: Iservicos3):
+    def __init__(self, servico_s3: Iservicos3, servico_banco: IoperacoesBanco):
         super().__init__()
         self.__servico_s3 = servico_s3
         self.__caminho_data = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
         self.__caminho_arquivo = f'steam/bronze/reviews_steam/'
+        self.__servico_banco = servico_banco
+
 
     def executar_processo(self, contexto: Optional[Contexto] = None) -> bool:
         for dados in contexto.gerador_reviews_steam:
+            steamid_api = dados['author']['steamid']
+            timestamp_updated_api = dados['timestamp_updated']
 
-            caminho_completo = self.__caminho_arquivo + f'jogo_{dados["codigo_steam"]}/' + f'data_{self.__caminho_data}' + '_reviews.json'
-            self.__servico_s3.guardar_dados(dados, caminho_completo)
+            condicao = f"author.steamid = {steamid_api} AND timestamp_updated = {timestamp_updated_api}"
+            caminho_consulta = "s3://extracao/steam/bronze/reviews_steam/" + f'jogo_{dados["codigo_steam"]}/' + '*.json'
+
+            dataframe = self.__servico_banco.consultar_dados(caminho_consulta=caminho_consulta,id_consulta=condicao)
+            if  dataframe.empty:
+                caminho_completo = self.__caminho_arquivo + f'jogo_{dados["codigo_steam"]}/' + f'data_{self.__caminho_data}' + '_reviews.json'
+                self.__servico_s3.guardar_dados(dados, caminho_completo)
+            else:
+                logger.info(f'{steamid_api} não teve atualizacao')
         return True
