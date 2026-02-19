2ebb056f62175923c5a3bba5df384a270701a1ec
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-18 23:18:54 -0300

    fix: correção

--- src/servicos/estrategia_tratamento/processador_texto.py
+++ src/servicos/estrategia_tratamento/processador_texto.py
@@ -1,25 +1,33 @@
-from typing import Generic, TypeVar, Optional
-
+from typing import Generic, TypeVar, Optional, Union, List
 from src.servicos.estrategia_tratamento.itratatamento import ITratamento
 
 T = TypeVar("T")
-U = TypeVar("U")
 
-
-class ProcessadorTexto(Generic[T, U ]):
+class ProcessadorTexto(Generic[T]):
 
     def __init__(self):
-        self._estrategia: Optional[ITratamento[T, U]] = None
+        self._estrategia: Optional[ITratamento[T]] = None
 
     @property
-    def estrategia(self) -> Optional[ITratamento[T, U]]:
+    def estrategia(self) -> Optional[ITratamento[T]]:
         return self._estrategia
 
     @estrategia.setter
-    def estrategia(self, nova_estrategia: ITratamento[T, U]) -> None:
+    def estrategia(self, nova_estrategia: ITratamento[T]) -> None:
         self._estrategia = nova_estrategia
 
-    def processar(self, comentario: T) -> U:
+    def processar(self, dados: Union[str, List[str]]) -> Union[T, List[T]]:
         if self._estrategia is None:
             raise ValueError("Estratégia de tratamento não foi definida.")
-        return self._estrategia.executar_tratamento(comentario)
+
+        if isinstance(dados, str):
+            self._estrategia.comentario = dados
+            return self._estrategia.executar_tratamento()
+        elif isinstance(dados, list):
+            resultados: List[T] = []
+            for item in dados:
+                self._estrategia.comentario = item
+                resultados.append(self._estrategia.executar_tratamento())
+            return resultados
+        else:
+            raise TypeError("Dados devem ser str ou List[str]")
