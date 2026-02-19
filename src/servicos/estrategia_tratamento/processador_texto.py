673b95f85e73d1f84a8d9b1d5087317a95e8b2fe
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-18 23:35:28 -0300

    Revert "Revert "Revert "fix: correção"""
    This reverts commit ae3bb6db534b4f52fff111a3073ecca5e1994fcb.

--- src/servicos/estrategia_tratamento/processador_texto.py
+++ src/servicos/estrategia_tratamento/processador_texto.py
@@ -1,33 +1,25 @@
-from typing import Generic, TypeVar, Optional, Union, List
+from typing import Generic, TypeVar, Optional
+
 from src.servicos.estrategia_tratamento.itratatamento import ITratamento
 
 T = TypeVar("T")
+U = TypeVar("U")
 
-class ProcessadorTexto(Generic[T]):
+
+class ProcessadorTexto(Generic[T, U ]):
 
     def __init__(self):
-        self._estrategia: Optional[ITratamento[T]] = None
+        self._estrategia: Optional[ITratamento[T, U]] = None
 
     @property
-    def estrategia(self) -> Optional[ITratamento[T]]:
+    def estrategia(self) -> Optional[ITratamento[T, U]]:
         return self._estrategia
 
     @estrategia.setter
-    def estrategia(self, nova_estrategia: ITratamento[T]) -> None:
+    def estrategia(self, nova_estrategia: ITratamento[T, U]) -> None:
         self._estrategia = nova_estrategia
 
-    def processar(self, dados: Union[str, List[str]]) -> Union[T, List[T]]:
+    def processar(self, comentario: T) -> U:
         if self._estrategia is None:
             raise ValueError("Estratégia de tratamento não foi definida.")
-
-        if isinstance(dados, str):
-            self._estrategia.comentario = dados
-            return self._estrategia.executar_tratamento()
-        elif isinstance(dados, list):
-            resultados: List[T] = []
-            for item in dados:
-                self._estrategia.comentario = item
-                resultados.append(self._estrategia.executar_tratamento())
-            return resultados
-        else:
-            raise TypeError("Dados devem ser str ou List[str]")
+        return self._estrategia.executar_tratamento(comentario)
