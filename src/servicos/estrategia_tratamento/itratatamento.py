a63e5d6a8ebcfd0852ce58943cb035663db62e85
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-18 23:35:28 -0300

    Revert "Revert "Revert "Revert "fix: correção""""
    This reverts commit daa22f309f36036250a67d4d0d4756aeab519b1c.

--- src/servicos/estrategia_tratamento/itratatamento.py
+++ src/servicos/estrategia_tratamento/itratatamento.py
@@ -3,6 +3,15 @@ from typing import Protocol, TypeVar
 E = TypeVar("E", contravariant=True)
 T = TypeVar("T", covariant=True)
 
-class ITratamento(Protocol[E, T]):
-    def executar_tratamento(self, comentario: E) -> T:
+
+class ITratamento(Protocol[T]):
+    @property
+    def comentario(self) -> str:
+        ...
+
+    @comentario.setter
+    def comentario(self, novo_comentario: str) -> None:
+        ...
+
+    def executar_tratamento(self) -> T:
         ...
